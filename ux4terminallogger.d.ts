8267e9fc6a6822a8c21d54eb6108d8718ae37342
ux4dev <42470394+ux4dev@users.noreply.github.com>
2019-06-18 12:52:03 +0100

    New d.ts Types

--- ux4terminallogger.d.ts
+++ ux4terminallogger.d.ts
@@ -2,44 +2,45 @@
 
 export = ux4terminallogger;
 
+interface Font {
+    bg_blue: string;
+    bg_cyan: string;
+    bg_green: string;
+    bg_magenta: string;
+    bg_red: string;
+    bg_white: string;
+    bg_yellow: string;
+    bold_off: string;
+    bold_on: string;
+    cls: string;
+    fg_black: string;
+    fg_blue: string;
+    fg_cyan: string;
+    fg_green: string;
+    fg_magenta: string;
+    fg_white: string;
+    fg_yellow: string;
+    inverse_on: string;
+    moveback1char: string;
+    moveback1line: string;
+    reset: string;
+}
+
 declare const ux4terminallogger: {
-    Font: {
-        bg_blue: string;
-        bg_cyan: string;
-        bg_green: string;
-        bg_magenta: string;
-        bg_red: string;
-        bg_white: string;
-        bg_yellow: string;
-        bold_off: string;
-        bold_on: string;
-        cls: string;
-        fg_black: string;
-        fg_blue: string;
-        fg_cyan: string;
-        fg_green: string;
-        fg_magenta: string;
-        fg_red: string;
-        fg_white: string;
-        fg_yellow: string;
-        inverse_on: string;
-        moveback1char: string;
-        moveback1line: string;
-        reset: string;
-    };
+    Font: Font;
     Log: {
-        applyStyle: any;
+        applyStyle(color: Font): any;
         cls: any;
-        debug: any;
+        debug(text: string, color: Font): any;
         disableColor: any;
-        fail: any;
-        info: any;
+        fail(text: string): any;
+        info(text: string): any;
         lf: any;
-        majorError: any;
+        majorError(text: string): any;
         showDebug: boolean;
-        success: any;
-        title: any;
-        warn: any;
+        success(text: string): any;
+        title(text: string, color: Font): any;
+        warn(text: string): any;
     };
 };
 
